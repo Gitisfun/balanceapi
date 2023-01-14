11df761d09ea99a50c10965dda566fb281ebe5d3
Gitisfun <christianvandenputtelaar@gmail.com>
2023-01-14 15:56:26 +0100

    Add middleware to routes

---
+++ middleware/authenticator.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+
+export default function (req, res, next) {
+  if (req.headers["authorization"]) {
+    const bearerHeader = req.headers["authorization"];
+    const token = bearerHeader.split(" ")[1];
+    if (token == null) return res.status(401).send({ msg: "No access allowed" });
+
+    // TODO: Add proper secret key
+    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+      if (err) {
+        res.status(403).send({ msg: "You don't have access to this route" });
+      } else {
+        req.user_id = user.foundUser.id;
+        next();
+      }
+    });
+  } else {
+    return res.status(403).send({ msg: "You don't have access to this route" });
+  }
+}
