063d8caeff6a3e9a0b42e90a064bad7251a68fe8
Gitisfun <christianvandenputtelaar@gmail.com>
2023-02-06 22:56:27 +0100

    Removed unnecessary method

--- logic/ParamBuilder.js
+++ logic/ParamBuilder.js
@@ -26,16 +26,6 @@ class ParamsBuilder {
     }
     return paramList;
   }
-
-  static rangeStartAndEnd(req) {
-    let paramList = [];
-    for (let i = 0; i < req.body.length; i++) {
-      paramList.push(req.userId);
-      paramList.push(req.body[i].start_date);
-      paramList.push(req.body[i].end_date);
-    }
-    return paramList;
-  }
 }
 
 export default ParamsBuilder;
